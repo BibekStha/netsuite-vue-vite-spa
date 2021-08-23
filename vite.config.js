0f149efef5976e3916775822b26b85a59e67018c
Bibek Shrestha <bibekmanshrestha@gmail.com>
2021-08-23 15:36:26 -0400

    use a plugin to inline all JS/CSS to a single HTML file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(), viteSingleFile()],
+  build: {
+    target: "es2015",
+    assetsInlineLimit: 100000000,
+    chunkSizeWarningLimit: 100000000,
+    cssCodeSplit: false,
+    brotliSize: false,
+    rollupOptions: {
+      inlineDynamicImports: true,
+      output: {
+        manualChunks: () => "everything.js",
+      },
+    },
+  },
 })
