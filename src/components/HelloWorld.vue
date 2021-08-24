73c9c2dd366cfde68fe54267d828e9e9c53f2779
Bibek Shrestha <bibekmanshrestha@gmail.com>
2021-08-24 11:02:36 -0400

    replace default text with text related to NetSuite

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,21 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>NetSuite Vue-Vite SPA Test</h1>
 
+  <button type="button" @click="state.count++">count is: {{ state.count }}</button>
   <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="state.count++">
-    count is: {{ state.count }}
-  </button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
+    Edit or delete
+    <code>components/HelloWorld.vue</code> to start creating your SPA for NetSuite.
   </p>
 </template>
 
 <script setup>
-import { defineProps, reactive } from 'vue'
+import { defineProps, reactive } from "vue";
 
 defineProps({
-  msg: String
-})
+  msg: String,
+});
 
-const state = reactive({ count: 0 })
+const state = reactive({ count: 0 });
 </script>
 
 <style scoped>
