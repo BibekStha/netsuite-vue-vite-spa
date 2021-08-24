b919b6f96aff223a0f27970389920b42f1c31bd7
Bibek Shrestha <bibekmanshrestha@gmail.com>
2021-08-24 15:33:04 -0400

    change the purge config for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./src/**/*.html", "./src/**/*.vue"],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
