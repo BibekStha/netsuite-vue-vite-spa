b32360e06cd47df3f37e00952703be783a239328
Bibek Shrestha <bibekmanshrestha@gmail.com>
2021-08-24 15:33:47 -0400

    add some stylings using TailwindCSS

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,13 @@
 <template>
-  <h1>NetSuite Vue-Vite SPA Test</h1>
+  <div class="flex flex-col justify-center items-center min-h-screen font-sans text-gray-700">
+    <h1 class="font-thin text-3xl my-10">NetSuite Vue-Vite SPA Test</h1>
 
-  <button type="button" @click="state.count++">count is: {{ state.count }}</button>
-  <p>
-    Edit or delete
-    <code>components/HelloWorld.vue</code> to start creating your SPA for NetSuite.
-  </p>
+    <button type="button" @click="state.count++" class="shadow px-5 py-2 rounded-xl my-5">count is: {{ state.count }}</button>
+    <p class="my-5">
+      Edit or delete
+      <code class="font-mono text-black">components/HelloWorld.vue</code> to start creating your SPA for NetSuite.
+    </p>
+  </div>
 </template>
 
 <script setup>
